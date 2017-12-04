1fc312f3ed584ba8f087eb6061f8eb03fed27d1a
Frank Ge <fangwg@uw.edu>
2017-12-03 18:41:26 -0800

    minor adjustments

--- ui.R
+++ ui.R
@@ -62,6 +62,105 @@ fluidPage(
                  scatterD3Output("stats", height = "600px")
                )
              )
+    ), 
+    
+    tabPanel("About",
+             tags$h1(
+               "Pokemon Stats Exploration"
+             ),
+             
+             tags$h2(
+               "The Project"
+             ),
+             
+             tags$div(
+               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                   Integer nec odio. Praesent libero. Sed cursus ante dapibus 
+               diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. 
+               Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed 
+               augue semper porta. Mauris massa. Vestibulum lacinia arcu 
+               eget nulla. Class aptent taciti sociosqu ad litora torquent 
+               per conubia nostra, per inceptos himenaeos. Curabitur 
+               sodales ligula in libero."
+             ),
+             
+             tags$h2(
+               "The Data"
+             ),
+             
+             tags$div(
+               "Data Source:",
+               tags$a(
+                 "https://www.kaggle.com/abcsds/pokemon"
+               )
+             ),
+             
+             tags$h2(
+               "Contributions"
+             ),
+             
+             tags$ul(
+               tags$li(
+                 tags$b("Eric S Shin"),
+                 tags$p(
+                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                   Integer nec odio. Praesent libero. Sed cursus ante dapibus 
+                   diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. 
+                   Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed 
+                   augue semper porta. Mauris massa. Vestibulum lacinia arcu 
+                   eget nulla. Class aptent taciti sociosqu ad litora torquent 
+                   per conubia nostra, per inceptos himenaeos. Curabitur 
+                   sodales ligula in libero."
+                 )
+               ),
+               
+               tags$li(
+                 tags$b("Eun Won Park"),
+                 tags$p(
+                   "Class aptent taciti sociosqu ad litora torquent per conubia 
+                   nostra, per inceptos himenaeos. Curabitur sodales ligula in 
+                   libero. Sed dignissim lacinia nunc. Curabitur tortor. 
+                   Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. 
+                   Maecenas mattis. Sed convallis tristique sem. Proin ut 
+                   ligula vel nunc egestas porttitor. Morbi lectus risus, 
+                   iaculis vel, suscipit quis, luctus non, massa. Fusce ac 
+                   turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus 
+                   metus, ullamcorper vel, tincidunt sed, euismod in, nibh. "
+                 )
+               ),
+               
+               tags$li(
+                 tags$b("Frank Ge"),
+                 tags$p(
+                   "Quisque volutpat condimentum velit. Class aptent taciti 
+                   sociosqu ad litora torquent per conubia nostra, per inceptos 
+                   himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt 
+                   mattis, tortor neque adipiscing diam, a cursus ipsum ante quis 
+                   turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. 
+                   Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. 
+                   Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus 
+                   suscipit. "
+                 )
+               ),
+               
+               tags$li(
+                 tags$b("Gilbert Febrianto"),
+                 tags$p(
+                   "Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, 
+                   metus vitae pharetra auctor, sem massa mattis sem, at interdum 
+                   magna augue eget diam. Vestibulum ante ipsum primis in faucibus 
+                   orci luctus et ultrices posuere cubilia Curae; Morbi lacinia 
+                   molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit 
+                   amet augue congue elementum. Morbi in ipsum sit amet pede 
+                   facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, 
+                   egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut 
+                   ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui 
+                   quis est pulvinar ullamcorper. "
+                 )
+               )
+               
+             )
+             
     )
   )
 )
